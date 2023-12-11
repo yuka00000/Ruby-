827d39bdb2995f4a34d93a25b8d76b9f5e19a789
yuka00000 <yukayamada0914@gmail.com>
2023-12-11 08:57:47 +0000

    テストコード分離

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,4 +1,3 @@
-
 def fizz_buzz(n)
   if n % 15 == 0
     'Fizz Buzz'
@@ -10,18 +9,3 @@ def fizz_buzz(n)
     n.to_s
   end
 end
-
-require 'minitest/autorun'
-
-class FizzBuzzTest < Minitest::Test
-  def test_fizz_buzz
-    assert_equal '1', fizz_buzz(1)
-    assert_equal '2', fizz_buzz(2)
-    assert_equal 'Fizz', fizz_buzz(3)
-    assert_equal '4', fizz_buzz(4)
-    assert_equal 'Buzz', fizz_buzz(5)
-    assert_equal 'Fizz', fizz_buzz(6)
-    assert_equal 'Fizz Buzz', fizz_buzz(15)
-  end
-end
-
